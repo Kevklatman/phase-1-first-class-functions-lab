2dbbb9e900b236461017e19d4149417c58121671
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-07-15 14:22:36 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function([]){
+   return ['Antonia', 'Nuru', 'Amari', 'Mo'].slice(0, 2);
+}
+const returnLastTwoDrivers = function([]){
+    return ['Antonia', 'Nuru', 'Amari', 'Mo'].slice(2);
+ }
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (){
+    return function(fare){
+       return fare * 5
+    }
+    }
+const fareDoubler = function(fare){
+    return fare * 2;
+    }
+const fareTripler = function(fare){
+    return fare * 3;
+    }
+const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+function selectDifferentDrivers(arrayOfDrivers, selectingDrivers) {
+    return selectingDrivers(arrayOfDrivers);
+}
